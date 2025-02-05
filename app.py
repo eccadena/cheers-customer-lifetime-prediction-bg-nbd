9458cb54d79baf963c54c9fafd6b7a21192da594
Eric <eccadena@gmail.com>
2025-02-04 21:43:38 -0600

    last commit to clean the outputs and the readme

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:025018047649b6ebca863cafd1a92bdf96fd262af4b415e91e5a8cfcda2c1631
-size 2153
+oid sha256:109573ce8a7b25cfa3193e4c70e23f4ad9bc1056ab21ba72f6b6c3efabced649
+size 2264
--- utils/eda.py
+++ utils/eda.py
@@ -1,25 +1,3 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-def plot_rfm_distributions(rfm_df):
-    """Plot distributions of Recency, Frequency, and Monetary Value."""
-    plt.figure(figsize=(15, 5))
-
-    # Recency Distribution
-    plt.subplot(1, 3, 1)
-    sns.histplot(rfm_df['Recency'], bins=30, kde=True)
-    plt.title('Recency Distribution')
-    
-    # Frequency Distribution
-    plt.subplot(1, 3, 2)
-    sns.histplot(rfm_df['Frequency'], bins=30, kde=True)
-    plt.title('Frequency Distribution')
-    
-    # Monetary Value Distribution
-    plt.subplot(1, 3, 3)
-    sns.histplot(rfm_df['MonetaryValue'], bins=30, kde=True)
-    plt.title('Monetary Value Distribution')
-    
-    plt.tight_layout()
-    plt.savefig('outputs/eda_visualizations/rfm_distributions.png')
-    plt.show()
+version https://git-lfs.github.com/spec/v1
+oid sha256:648b7d834268ea5548535472e1dfa536a4fa1abe0e08368aa3b2f2770f260e9c
+size 1450
